d04087fa56b3047d653bbbe21445f30c871bd7fc
MatheusTamulis <matheustamulis@gmail.com>
2023-07-07 16:07:38 -0300

    create the carrossel functional js

--- js/components/main-carrossel.js
+++ js/components/main-carrossel.js
@@ -1,1 +1,26 @@
-alert("Ta funfando");
+// Acessar a Janela (browser)
+// Pegar o HTML
+// Pegar o botão
+// Saber que está sendo clicado no botão
+
+
+// const não pode se mudar o valor = constante
+const buttonRight = window.document.querySelector('.button-arrow.-right');  //Pode se usar o Window.document ou só document
+const buttonLeft = window.document.querySelector('.button-arrow.-left');
+const elements = document.querySelector('.elements');
+// let pode mudar o valor da variavel 
+let pixel = 0;
+
+// console.log(buttonRight)  MOSTRA NO CONSOLE DO INSPECIONAR ELEMENTO SE ESTA CERTO
+
+buttonRight.addEventListener('click', function() 
+{
+    pixel = pixel + 50;
+    elements.style = `transform: translateX(${pixel}px);`;
+});
+
+buttonLeft.addEventListener('click', function () 
+{
+    pixel = pixel - 50;
+    elements.style = `transform: translateX(${pixel}px);`;
+});
